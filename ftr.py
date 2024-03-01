ca71d8c0457d5bc0913d754676ed4967bc6b8612
Slientboyx <161596052+Slientboyx@users.noreply.github.com>
2024-03-01 17:17:25 +0500

    Update ftr.py

--- ftr.py
+++ ftr.py
@@ -53,9 +53,9 @@ start += [[custom.Button.url("Add me to your group", "t.me/Seona_RoBot?=start")]
 @bot.on(events.NewMessage(pattern="^/pin$"))
 async def pin(event):
 
-    chat_id = event.chat_id
-    reply_msg = get_reply_messsage()
-    message = get_reply_messsage()
+    chat_id = await event.chat_id
+    reply_msg = await event.get_reply_messsage()
+    message = await event.get_reply_messsage()
     if not event.reply_msg:
         await event.reply(
             "You need to reply to a message to pin it!"
